0fbe891bb60177bfe7b3d2698a07e7e6fa3b5ea7
chandu velagapudi <chandu.velagapudi22@gmail.com>
2021-05-11 10:43:35 +0530

    This is Frist Commit i did in HydHostals

---
+++ src/com/info/hyd/HydHostelData.java
@@ -0,0 +1,19 @@
+package com.info.hyd;
+
+public class HydHostelData {
+	public void food() {
+		System.out.println("This is food() in hyd hostal");
+	}
+
+	public void hostal_Beds() {
+		System.out.println("This is hostal_Beds() methods in HydHostelData  ");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome chandu velagapudi");
+		HydHostelData hostelData = new HydHostelData();
+		hostelData.food();
+
+	}
+
+}
